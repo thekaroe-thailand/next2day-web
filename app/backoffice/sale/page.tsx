217a50c99ce549d96137e2a7786affc881c119d8
thekaroe-thailand <thekaroe@gmail.com>
2025-01-19 14:43:13 +0700

    style table

--- app/backoffice/sale/page.tsx
+++ app/backoffice/sale/page.tsx
@@ -7,8 +7,39 @@ import Swal from 'sweetalert2';
 
 export default function Page() {
     const [barcode, setBarcode] = useState("");
+    const [orderItems, setOrderItems] = useState([]); // รายการสินค้าที่ขาย
 
-    const handleSave = async () => { }
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const url = `${ApiUrl}/api/sale/list`;
+            const response = await axios.get(url);
+            setOrderItems(response.data);
+        } catch (err: any) {
+            Swal.fire({
+                title: 'error',
+                text: err.message,
+                icon: 'error'
+            })
+        }
+    }
+
+    const handleSave = async () => {
+        try {
+            const url = `${ApiUrl}/api/sale/save`;
+            const payload = { barcode: barcode }
+            await axios.post(url, payload)
+        } catch (err: any) {
+            Swal.fire({
+                title: 'error',
+                text: err.message,
+                icon: 'error'
+            })
+        }
+    }
 
     return (
         <div>
@@ -20,6 +51,25 @@ export default function Page() {
                     บันทึก
                 </button>
             </div>
+
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>barcode</th>
+                        <th>ชื่อสินค้า</th>
+                        <th className="text-right">ราคา</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {orderItems.map((item: any) => (
+                        <tr key={item.id}>
+                            <td>{item.book.barcode}</td>
+                            <td>{item.book.name}</td>
+                            <td className="text-right">{item.price}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
